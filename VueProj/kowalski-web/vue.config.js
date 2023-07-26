4823034289128c781ce7e1e514b8b7e073824f43
Kowalski <rei_0_0@qq.com>
2023-07-26 20:30:55 +0800

    引用element-ui 把主页index拆分成三大块去管理 把同步笔记文档加入到仓库

--- VueProj/kowalski-web/vue.config.js
+++ VueProj/kowalski-web/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false //不去检查语法，否则非驼峰式命名的文件会报错
 })
